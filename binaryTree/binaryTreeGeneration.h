7361e2308241fecf3c8425287847b8a3ccb7ce09
Vishal Dogra <dogravishal1598@gmail.com>
2022-09-18 09:17:10 +0530

    added logic to generate binary tree recursively

--- binaryTree/binaryTreeGeneration.h
+++ binaryTree/binaryTreeGeneration.h
@@ -3,48 +3,65 @@
 using namespace std;
 
 class TreeNode {
-   public:
-    int data;
-    TreeNode* left;
-    TreeNode* right;
+ public:
+  int data;
+  TreeNode* left;
+  TreeNode* right;
 
-    TreeNode(int data) {
-        this->data = data;
-        this->left = nullptr;
-        this->right = nullptr;
-    }
+  TreeNode(int data) {
+    this->data = data;
+    this->left = nullptr;
+    this->right = nullptr;
+  }
 };
 
 void insertNode(TreeNode*& root, int data) {
-    TreeNode* newNode = new TreeNode(data);
-    if(!newNode) {
-        cout << "Memory Error\n";
-        return;
+  TreeNode* newNode = new TreeNode(data);
+  if (!newNode) {
+    cout << "Memory Error\n";
+    return;
+  }
+
+  if (!root) {
+    root = newNode;
+    return;
+  }
+
+  queue<TreeNode*> q;
+  q.push(root);
+  while (!q.empty()) {
+    TreeNode* currNode = q.front();
+    q.pop();
+
+    if (!currNode->left) {
+      currNode->left = newNode;
+      return;
+    } else {
+      q.push(currNode->left);
     }
 
-    if (!root) {
-        root = newNode;
-        return;
+    if (!currNode->right) {
+      currNode->right = newNode;
+      return;
+    } else {
+      q.push(currNode->right);
     }
+  }
+}
 
-    queue<TreeNode*> q;
-    q.push(root);
-    while (!q.empty()) {
-        TreeNode* currNode = q.front();
-        q.pop();
+// Building tree recursive
+TreeNode* buildTree(TreeNode* root) {
+  cout << "Enter data: ";
+  int data;
+  cin >> data;
+  root = new TreeNode(data);
 
-        if (!currNode->left) {
-            currNode->left = newNode;
-            return;
-        } else {
-            q.push(currNode->left);
-        }
+  if (data == -1) return nullptr;
 
-        if (!currNode->right) {
-            currNode->right = newNode;
-            return;
-        } else {
-            q.push(currNode->right);
-        }
-    }
+  cout << "To insert data in left of " << data << " ";
+  root->left = buildTree(root->left);
+  cout << "To insert data in right of " << data << " ";
+  root->right = buildTree(root->right);
+
+  return root;
 }
